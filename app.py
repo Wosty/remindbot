852d6b3060e4fb4395baac9d75000924530fbc7b
Wosty <36647710+Wosty@users.noreply.github.com>
2018-06-08 19:01:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def webhook():
 
   # We don't want to reply to ourselves!
   if data['name'] != 'wosty boi':
-    msg = '{}'.format(data['text'])
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
     send_message(msg)
 
   return "ok", 200
