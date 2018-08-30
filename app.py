0754c12a67b2b79fc7da149def090b0c6076728c
Wosty <36647710+Wosty@users.noreply.github.com>
2018-08-29 23:40:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,50 @@ def webhook():
     time.sleep(2)
     msg = 'MSC: Mark alec sophia \nCommons: Jackson Meg Wesley \nHarrington: Saul Cameron'
     send_message(msg)
+  else if data['text'] == '8:30':
+    time.sleep(2)
+    msg = 'MSC: Mark alec sophia \nCommons: Jackson Meg Wesley \nHarrington: Sidney Saul Cameron '
+    send_message(msg)
+  else if data['text'] == '9:00':
+    time.sleep(2)
+    msg = 'MSC: Jacob  alec Cameron \nCommons: Meg Wesley Jackson \nHarrington: Lexi Zac Saul '
+    send_message(msg)
+  else if data['text'] == '9:30':
+    time.sleep(2)
+    msg = 'MSC: Aly Oriana Jacob Cameron\nCommons: Meg Jackson alec \nHarrington: Lexi Zac Noah Saul'
+    send_message(msg)
+  else if data['text'] == '10:00':
+    time.sleep(2)
+    msg = 'MSC: Aly Oriana Jacob Zac\nCommons: Meg Jackson alec Jarrod\nHarrington: Addie Gracie Noah Saul'
+    send_message(msg)
+  else if data['text'] == '10:30':
+    time.sleep(2)
+    msg = 'MSC: Aly Oriana  \nCommons: Meg alec Jarrod \nHarrington:  Addie Gracie Noah Cameron'
+    send_message(msg)
+  else if data['text'] == '11:00':
+    time.sleep(2)
+    msg = 'MSC: Aly Oriana Cole \nCommons: Meg Jarrod alec \nSbisa:  Gracie Cameron Danya'
+    send_message(msg)
+  else if data['text'] == '11:30':
+    time.sleep(2)
+    msg = 'MSC: Aly Oriana Sophia Cole\nCommons: Meg Jarrod Alec \nSbisa:  Gracie Cameron Danya'
+    send_message(msg)
+  else if data['text'] == '12:00':
+    time.sleep(2)
+    msg = 'MSC: Sophia Cole Mark \nCommons: Jarrod  Meg \nSbisa:  Gracie Danya Cameron'
+    send_message(msg)
+  else if data['text'] == '12:30':
+    time.sleep(2)
+    msg = 'MSC: Cole Mark Sophia \nCommons: Jarrod Meg  \nSbisa:  Gracie Danya Cameron'
+    send_message(msg)
+  else if data['text'] == '1:00':
+    time.sleep(2)
+    msg = 'MSC: Cole Jacob Mark \nCommons: Addie Jarrod Saul \nSbisa:  Haley Danya Noah Cameron'
+    send_message(msg)
+  else if data['text'] == '1:30':
+    time.sleep(2)
+    msg = 'MSC: Cole Jacob Sidney \nCommons: Addie Jarrod Saul \nSbisa:  Haley Danya Noah Cameron'
+    send_message(msg)
 
   return "ok", 200
 
