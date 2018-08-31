2442f0a6defcea480ddef17a1a651457e061c53d
Wosty <36647710+Wosty@users.noreply.github.com>
2018-08-31 10:39:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,63 +58,70 @@ def webhook():
     time.sleep(2)
     msg = msg + 'MSC: Andrew Bre Gabi \nCommons: Kourtnei Oriana Mark \n Sbisa: Addie Jarrod'
     send_message(msg)
-  elif data['text'] == '1:00':
+  elif data['text'] == '1:00' or data['text'] == '13:00':
     time.sleep(2)
     msg = msg + 'MSC: Andrew Landon Thayer Gabi \nCommons: Kourtnei Aly Oriana \n Sbisa: Lexi Haley Jonathan'
     send_message(msg)
-  elif data['text'] == '1:30':
+  elif data['text'] == '1:30' or data['text'] == '13:30':
     time.sleep(2)
     msg = msg + 'MSC: Andrew Landon Thayer Gabi \nCommons: Kourtnei Aly Oriana \n Sbisa: Lexi Haley Sidney'
     send_message(msg)
-  elif data['text'] == '2:00':
+  elif data['text'] == '2:00' or data['text'] == '14:00':
     time.sleep(2)
     msg = msg + 'MSC: Andrew Landon Thayer Gabi Jacob\nCommons: Kourtnei Aly Bre Oriana\nHarrington: Creel Wesley Lexi Torey'
     send_message(msg)
-  elif data['text'] == '2:30':
+  elif data['text'] == '2:30' or data['text'] == '14:30':
     time.sleep(2)
     msg = msg + 'MSC: Mary Kate Landon Thayer Gabi Jacob\nCommons: Kourtnei Aly Bre Oriana\nHarrington: Creel Wesley Torey Saul'
     send_message(msg)
-  elif data['text'] == '3:00':
+  elif data['text'] == '3:00' or data['text'] == '15:00':
     time.sleep(2)
     msg = msg + 'MSC: Oriana Landon Thayer Jacob Mary Kate\nCommons: Bre Cameron Gabi Sidney\nHarrington: Addie Gracie Wesley Saul'
     send_message(msg)
-  elif data['text'] == '3:30':
+  elif data['text'] == '3:30' or data['text'] == '15:30':
     time.sleep(2)
     msg = msg + 'MSC: Oriana Landon Thayer Jonathan Mary Kate\nCommons: Bre Cameron Gabi Creel\nHarrington: Addie Gracie Wesley Saul'
     send_message(msg)
-  elif data['text'] == '4:00':
+  elif data['text'] == '4:00' or data['text'] == '16:00':
     time.sleep(2)
     msg = msg + 'MSC: Addie Landon Thayer Jonathan Mary Kate\nCommons: Creel Cameron Gabi Noah\nHarrington: Oriana Jarrod Gracie Saul'
+    if data['text'] == '16:00':
+      msg = msg + '\nF*** you, Landon C.'
     send_message(msg)
-  elif data['text'] == '4:30':
+  elif data['text'] == '4:30' or data['text'] == '16:30':
     time.sleep(2)
     msg = msg + 'MSC: Addie Landon Thayer Andrew Mary Kate\nCommons: Creel Cameron Gabi Noah\nHarrington: Oriana Jarrod Gracie Saul\n'
     send_message(msg)
-  elif data['text'] == '5:00':
+  elif data['text'] == '5:00' or data['text'] == '17:00':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-  elif data['text'] == '5:30':
+  elif data['text'] == '5:30' or data['text'] == '17:30':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-  elif data['text'] == '6:00':
+  elif data['text'] == '6:00' or data['text'] == '18:00':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-  elif data['text'] == '6:30':
+  elif data['text'] == '6:30' or data['text'] == '18:30':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-  elif data['text'] == '7:00':
+  elif data['text'] == '7:00' or data['text'] == '19:00':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-  elif data['text'] == '7:30':
+  elif data['text'] == '7:30' or data['text'] == '19:30':
     time.sleep(2)
     msg = 'Y\'all done hoes'
     send_message(msg)
-
+  elif  data['text'] == '20:00' or data['text'] == '20:30' or data['text'] == '21:00' or data['text'] == '21:30' or data['text'] == '22:00' or data['text'] == '22:30' or data['text'] == '23:00' or data['text'] == '23:30' or data['text'] == '0:00' or data['text'] == '0:30'
+    time.sleep(2)
+    msg = 'Y\'all done hoes'
+    if data['text'] == '21:00'
+      msg = 'A A A A AAAAA\n' + msg
+    send_message(msg)
   return "ok", 200
 
 def send_message(msg):
